f2b05f551a7424c680d30ebca4fb162bbde68ce0
IgorNovozhilov <maildrakon@gmail.com>
2020-11-11 23:30:50 +0300

    getVideoTracks

--- video-test.js
+++ video-test.js
@@ -7,13 +7,15 @@ try {
     text.innerHTML = _screen
     navigator.mediaDevices.getUserMedia(videoConstraints)
         .then(stream => {
-            const [videoTrack] = stream.getTracks()
+            const [videoTrack] = stream.getVideoTracks()
 
+            const supported = navigator.mediaDevices.getSupportedConstraints()
             const capabilities = videoTrack.getCapabilities ? videoTrack.getCapabilities() : {}
             const settings = videoTrack.getSettings ? videoTrack.getSettings() : {}
 
-            text.innerHTML += '\n\n' + JSON.stringify(capabilities, null, '  ')
-            text.innerHTML += '\n\n' + JSON.stringify(settings, null, '  ')
+            text.innerHTML += '\n\nSupported constraints:' + JSON.stringify(supported, null, '  ')
+            text.innerHTML += '\n\nTrack capabilities:' + JSON.stringify(capabilities, null, '  ')
+            text.innerHTML += '\n\nTrack settings:' + JSON.stringify(settings, null, '  ')
 
             videoTrack.stop()
         })

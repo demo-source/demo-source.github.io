d007b59121160fdb9deb4b2b6060c037ab99d2e6
IgorNovozhilov <maildrakon@gmail.com>
2021-09-22 22:34:34 +0300

    +

--- mwc-dialog.v1.js
+++ mwc-dialog.v1.js
@@ -2,4 +2,5 @@ import { oom } from 'https://cdn.jsdelivr.net/npm/notml@latest/core.js'
 import 'https://unpkg.com/@material/mwc-dialog?module'
 
 oom(document.body, oom
-    .mwcDialog({ open: true, title: 'Test Title' }))
+    .mwcDialog({ open: true, heading: 'Test Title' }, oom
+        .span('Test Text')))

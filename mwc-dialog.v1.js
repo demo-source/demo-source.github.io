52cbf80ed4620605598f58bf5eb315fd026d970f
IgorNovozhilov <maildrakon@gmail.com>
2021-09-22 22:37:22 +0300

    +

--- mwc-dialog.v1.js
+++ mwc-dialog.v1.js
@@ -1,6 +1,9 @@
 import { oom } from 'https://cdn.jsdelivr.net/npm/notml@latest/core.js'
 import 'https://unpkg.com/@material/mwc-dialog?module'
 
+oom(document.head, oom
+    .link({ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap' }))
+
 oom(document.body, oom
     .mwcDialog({ open: true, heading: 'Test Title' }, oom
         .span('Test Text')))

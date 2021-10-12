fde77f6214a0d5955dc2bc3f8289dd91775e379a
IgorNovozhilov <maildrakon@gmail.com>
2021-10-12 23:51:41 +0300

    test

--- saby/turn-on-in-accordion.js
+++ saby/turn-on-in-accordion.js
@@ -1,1 +1,24 @@
+const accordionContainer = document.querySelector('.NavigationPanels-Accordion__container')
+const style = document.createElement('style')
+const container = document.createElement('div')
+const switcher = document.createElement('div')
+const config = document.createElement('div')
+
+container.setAttribute('data-vdomignore', 'true')
+container.classList.add('toia__container')
+switcher.classList.add('toia__switcher')
+config.classList.add('toia__config')
+
+document.head.append(style)
+container.append(switcher)
+container.append(config)
+accordionContainer.append(container)
+
 console.log('Turn on in Accordion - ok')
+
+requirejs([
+    'UI/Base',
+    'Controls/toggle'
+], (Base, toggle) => {
+    const switcherControl = Base.Control.createControl(toggle.Switch, { caption: 'Включатель' }, switcher)
+})
